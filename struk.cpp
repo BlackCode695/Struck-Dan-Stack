1a626481027949a3e0b8dd6b42d2ce6246e1fe53
Akhmad Hafiz Azhari <hafiz060905@gmail.com>
2025-06-03 00:35:53 +0800

    Penggunaan Pointer di Struck

--- struk.cpp
+++ struk.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
 
-int main(){
-struct mahasiswa{
+struct Mahasiswa
+{
     string nim;
     string nama;
     string alamat;
     float ipk;
 };
 
-mahasiswa Hafiz;
+int main(int argc, char const *argv[])
+{
+    Mahasiswa hafiz;
+    hafiz.nim = "C030324005";
+    hafiz.nama = "Hafiz";
+    hafiz.alamat = "Jl.Gunung sari";
+    hafiz.ipk = 3,5;
+    Mahasiswa Hafiz;
+    Mahasiswa *ptrHafiz = &Hafiz;
 
-Hafiz.nim="c030324005";
-Hafiz.nama="Hafiz";
-Hafiz.alamat="jl Gunung sari";
-Hafiz.ipk=3.5;
+    cout << "Nama: " << hafiz.nama << endl;
+    cout << "Alamat: " << hafiz.alamat << endl;
+    cout << "NIM: " << hafiz.nim << endl;
+    cout << "IPK: " << hafiz.ipk << endl;
 
-cout << Hafiz.nim<<"\n";
-cout << Hafiz.nama<<"\n";
-cout << Hafiz.alamat<<"\n";
-cout << Hafiz.ipk<<"\n";
+    cout << "Masukkan NIM: ";
+    cin >> ptrHafiz->nim;
+    system("cls");
+
+    cout << "Masukkan Nama: ";
+    cin >> ptrHafiz->nama;
+    system("cls");
+
+    cout << "Masukkan Alamat: ";
+    cin >> ptrHafiz->alamat;
+    system("cls");
+
+    cout << "Masukkan ipk: ";
+    cin >> ptrHafiz->ipk;
+    system("cls");
+
+    cout << "NIM: " << ptrHafiz->nim << endl;
+    cout << "Nama: " << ptrHafiz->nama << endl;
+    cout << "Alamat: " << ptrHafiz->alamat << endl;
+    cout << "IPK: " << ptrHafiz->ipk << endl;
+    return 0;
 }
